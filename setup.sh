1a1e719484cbf64ae9ced92f123e2f285a518185
Nathan Baltzell <baltzell@gmx.com>
2025-10-07 12:34:42 -0400

    append options

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${DIR}/build
-export JAVA_OPTS="-Djava.library.path=${DIR}/build"
+export JAVA_OPTS="${JAVA_OPTS} -Djava.library.path=${DIR}/build"
 export JYPATH="${DIR}/target/*"
